a0763c7b08763c5f57fb4f43fc0a9fafabbab42c
barnabegeffroy <barnabe.geffroy@ecomail.fr>
2024-11-16 17:17:17 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const NBR_VOEUX = 4;
+const NBR_SORTIES = 3;
 
-document.getElementById("processCsv").addEventListener("click", () => {
-  const fileInput = document.getElementById("csvFile");
+function processFile() {
+  const fileInput = document.getElementById("tsvFile");
   if (fileInput.files.length === 0) {
-    alert("Veuillez sélectionner un fichier CSV.");
+    alert("Veuillez sélectionner un fichier TSV.");
     return;
   }
 
@@ -11,16 +12,26 @@ document.getElementById("processCsv").addEventListener("click", () => {
   const reader = new FileReader();
 
   reader.onload = function (event) {
-    const csvContent = event.target.result;
-    const rows = csvContent
-      .split("\n")
-      .map((row) => row.split(",").map((cell) => cell.trim()));
+    const tsvContent = event.target.result;
 
-    findEvents(rows);
+    // Traitement du contenu TSV :
+    // 1. Séparer par ligne.
+    // 2. Séparer chaque ligne par la tabulation.
+    // 3. Enlever les guillemets autour des cellules.
+    const rows = tsvContent
+      .split("\n") // Sépare le fichier par ligne
+      .map(
+        (row) =>
+          row
+            .split("\t") // Sépare la ligne par tabulation
+            .map((cell) => cell.replace(/(^"|"$)/g, "").trim()) // Retire les guillemets autour des cellules et enlève les espaces inutiles
+      );
+
+    findEvents(rows); // Traitez les données après les avoir séparées
   };
 
   reader.readAsText(file);
-});
+}
 
 function findEvents(rows) {
   const premiumStartIndex = rows[1].indexOf("premium");
@@ -32,20 +43,19 @@ function findEvents(rows) {
 
   var premiumEvents = [];
   const autresEvents = [];
-  var users = {};
 
   // Créer les objets d'événements pour premium
   premiumBrut.forEach((eventBrut, _) => {
     const [index, name, capacity] = eventBrut.split("-");
+    console.log(name)
     const eventObject = {
       index: parseInt(index),
       name,
-      capacity: parseInt(capacity),
+      capacity: capacity,
     };
     premiumEvents.push(eventObject);
   });
-
-  console.log(JSON.stringify(premiumEvents));
+  console.log(premiumEvents)
 
   // Créer les objets d'événements pour autres
   autresBrut.forEach((eventBrut, _) => {
@@ -58,7 +68,45 @@ function findEvents(rows) {
     autresEvents.push(eventObject);
   });
 
+  // Variables pour stocker les classements
+  const premiumEventsOrdonnes = {};
+  const autresEventsOrdonnes = {};
+
+  // Récupérer les préférences de chaque utilisateur pour premium
+  for (let i = 3; i < rows.length; i++) {
+    const row = rows[i];
+    if (row.length > 0) {
+      const webformSerial = row[0];
+
+      var voeuxPremium = [];
+      var voeuxAutres = [];
+
+      const voeuxPremiumBrut = row
+        .slice(premiumStartIndex, autresStartIndex)
+        .map((item) => (item === "" ? "-1-" : parseInt(item, 10)));
+
+      const voeuxAutresBrut = row
+        .slice(autresStartIndex)
+        .map((item) => (item === "" ? "-1-" : parseInt(item, 10)));
+
+      for (let j = 0; j < NBR_VOEUX; j++) {
+        var premiumEventIndex = voeuxPremiumBrut.indexOf(j);
+        var autreEventIndex = voeuxAutresBrut.indexOf(j);
+        if (premiumEventIndex != -1) {
+          voeuxPremium.push(premiumEvents[premiumEventIndex]);
+          voeuxAutres.push(autresEvents[autreEventIndex]);
+        }
+      }
+
+      // Remplacer l'utilisation de la Map par un objet JSON
+      premiumEventsOrdonnes[webformSerial] = voeuxPremium;
+      autresEventsOrdonnes[webformSerial] = voeuxAutres;
+    }
+  }
+  console.log(premiumEventsOrdonnes[4])
+
   // Récupérer les infos de chaque utilisateur
+  var personnes = {};
   for (let i = 3; i < rows.length; i++) {
     const row = rows[i];
     if (row.length > 0) {
@@ -68,7 +116,7 @@ function findEvents(rows) {
         premiumStartIndex
       );
 
-      users[webformSerial] = {
+      personnes[webformSerial] = {
         nom: nom,
         prenom: prenom,
         coordonnees: coordonnees,
@@ -78,136 +126,279 @@ function findEvents(rows) {
     }
   }
 
-  // Variables pour stocker les classements
-  const autresEventsOrder = {};
-  const premiumEventsOrder = {};
+  // Assignation
 
-  // Récupérer les préférences de chaque utilisateur pour premium
-  for (let i = 3; i < rows.length; i++) {
-    const row = rows[i];
-    if (row.length > 0) {
-      const webformSerial = row[0];
-
-      var voeuxPremium = [];
-
-      const voeuxPremiumBrut = row
-        .slice(premiumStartIndex, autresStartIndex)
-        .map((item) => (item === "" ? "-1-" : parseInt(item, 10)));
-
-      for (let j = 0; j < NBR_VOEUX; j++) {
-        var eventIndex = voeuxPremiumBrut.indexOf(j);
-        if (eventIndex != -1) {
-          voeuxPremium.push(premiumEvents[eventIndex]);
-        }
-      }
-
-      // Remplacer l'utilisation de la Map par un objet JSON
-      premiumEventsOrder[webformSerial] = voeuxPremium;
-    }
-  }
-  const NBR_SORTIES = 3; // Limite du nombre de sorties par participant
-
-  let score = {};
-  let assignments = {};
-  let waitingList = {};
+  let scorePremium = {};
+  let assignmentsPremium = {};
+  let waitingListPremium = {};
 
   premiumEvents.forEach((event) => {
-    waitingList[event.name] = []; // Initialisation des listes d'attente pour chaque événement
+    waitingListPremium[event.name] = []; // Initialisation des listes d'attente pour chaque événement
   });
 
   // Initialiser les scores et les assignations
-  for (let participantId in premiumEventsOrder) {
-    score[participantId] = 0; // Score initial des participants
-    assignments[participantId] = []; // Assignation des événements aux participants
+  for (let participantId in premiumEventsOrdonnes) {
+    scorePremium[participantId] = 0; // Score initial des participants
+    assignmentsPremium[participantId] = []; // Assignation des événements aux participants
   }
 
   // Parcours des voeux de chaque participant, de 1 à NBR_VOEUX
   for (let voeuxNum = 0; voeuxNum < NBR_VOEUX; voeuxNum++) {
     // Trier les participants par leur score (prioriser les plus petits scores)
-    const sortedParticipants = Object.keys(score)
+    const sortedParticipants = Object.keys(scorePremium)
       .filter(
-        (participantId) => assignments[participantId].length < NBR_SORTIES
+        (participantId) =>
+          assignmentsPremium[participantId].length < NBR_SORTIES
       ) // Sélectionner ceux qui n'ont pas encore de score et n'ont pas atteint la limite de sorties
-      .sort((a, b) => score[a] - score[b]); // Trier par score croissant
+      .sort((a, b) => scorePremium[a] - scorePremium[b]); // Trier par score croissant
 
     for (let participantId of sortedParticipants) {
       try {
-        let event = premiumEventsOrder[participantId][voeuxNum];
+        let event = premiumEventsOrdonnes[participantId][voeuxNum];
         let eventName = event.name;
 
         // Vérifier la capacité de l'événement
         let eventCapacity = premiumEvents[event.index].capacity;
-        let minimumCapacity = users[participantId].binome == 1 ? 1 : 0;
+        let minimumCapacity = personnes[participantId].binome == 1 ? 1 : 0;
+        console.log(event)
         if (eventCapacity > minimumCapacity) {
           // Si le participant n'a pas encore de sortie, l'assigner à cet événement
-          if (assignments[participantId].length < NBR_SORTIES) {
-            assignments[participantId].push(eventName);
+          if (assignmentsPremium[participantId].length < NBR_SORTIES) {
+            assignmentsPremium[participantId].push(eventName);
             premiumEvents[event.index].capacity -=
-              users[participantId].binome == 1 ? 2 : 1; // Réduire la capacité de l'événement
-            score[participantId] += 4 - voeuxNum; // Calculer le score
+              personnes[participantId].binome == 1 ? 2 : 1; // Réduire la capacité de l'événement
+            scorePremium[participantId] += NBR_VOEUX - voeuxNum; // Calculer le score
           }
         } else {
           // Si l'événement est plein, l'ajouter à la liste d'attente
-          waitingList[eventName].push(participantId);
+          waitingListPremium[eventName].push(participantId);
         }
       } catch (error) {
         // Gérer les erreurs si nécessaire
       }
     }
   }
-  let participantsByEvent = {};
+  let participantsByEventPremium = {};
 
   // Parcourir les assignations pour remplir `participantsByEvent`
-  Object.keys(assignments).forEach((participantId) => {
-    assignments[participantId].forEach((eventName) => {
-      if (!participantsByEvent[eventName]) {
-        participantsByEvent[eventName] = [];
+  Object.keys(assignmentsPremium).forEach((participantId) => {
+    assignmentsPremium[participantId].forEach((eventName) => {
+      if (!participantsByEventPremium[eventName]) {
+        participantsByEventPremium[eventName] = [];
       }
-      participantsByEvent[eventName].push(participantId);
+      participantsByEventPremium[eventName].push(participantId);
     });
   });
 
-  //   console.log(JSON.stringify(users));
-  //   console.log(JSON.stringify(participantsByEvent));
-  //   console.log(JSON.stringify(assignments));
-  //   console.log(JSON.stringify(waitingList));
-  //   console.log(JSON.stringify(score));
+  let scoreAutres = {};
+  let assignmentsAutres = {};
+  let waitingListAutres = {};
 
-  // Construction des lignes du CSV
-  const csvHeader = [
+  autresEvents.forEach((event) => {
+    waitingListAutres[event.name] = []; // Initialisation des listes d'attente pour chaque événement
+  });
+
+  // Initialiser les scores et les assignations pour "autres"
+  for (let participantId in autresEventsOrdonnes) {
+    scoreAutres[participantId] = 0; // Score initial des participants
+    assignmentsAutres[participantId] = []; // Assignation des événements "autres"
+  }
+
+  // Parcours des voeux de chaque participant pour "autres"
+  for (let voeuxNum = 0; voeuxNum < NBR_VOEUX; voeuxNum++) {
+    const sortedParticipants = Object.keys(scoreAutres)
+      .filter(
+        (participantId) => assignmentsAutres[participantId].length < NBR_SORTIES
+      )
+      .sort((a, b) => scoreAutres[a] - scoreAutres[b]);
+
+    for (let participantId of sortedParticipants) {
+      try {
+        let event = autresEventsOrdonnes[participantId][voeuxNum];
+        let eventName = event.name;
+
+        let eventCapacity = autresEvents[event.index].capacity;
+        let minimumCapacity = personnes[participantId].binome == 1 ? 1 : 0;
+        if (eventCapacity > minimumCapacity) {
+          if (assignmentsAutres[participantId].length < NBR_SORTIES) {
+            assignmentsAutres[participantId].push(eventName);
+            autresEvents[event.index].capacity -=
+              personnes[participantId].binome == 1 ? 2 : 1;
+            scoreAutres[participantId] += NBR_VOEUX - voeuxNum;
+          }
+        } else {
+          waitingListAutres[eventName].push(participantId);
+        }
+      } catch (error) {
+        // Gérer les erreurs si nécessaire
+      }
+    }
+  }
+
+  let participantsByEventAutres = {};
+
+  Object.keys(assignmentsAutres).forEach((participantId) => {
+    assignmentsAutres[participantId].forEach((eventName) => {
+      if (!participantsByEventAutres[eventName]) {
+        participantsByEventAutres[eventName] = [];
+      }
+      participantsByEventAutres[eventName].push(participantId);
+    });
+  });
+
+  const headersXLS = [
     "Nom",
     "Prénom",
     "Coordonnées",
     "Binôme",
     "Coordonnées Binôme",
   ];
-  for (let i = 1; i <= NBR_SORTIES; i++) {
-    csvHeader.push(`Visite ${i}`);
-  }
-  const csvRows = [csvHeader];
 
-  Object.entries(users).forEach(([id, personne]) => {
+  for (let i = 1; i <= NBR_SORTIES; i++) {
+    headersXLS.push(`Premium - ${i}`);
+  }
+  for (let i = 1; i <= NBR_SORTIES; i++) {
+    headersXLS.push(`Autres - ${i}`);
+  }
+
+  const data = [headersXLS]; // Première ligne : les en-têtes
+
+  Object.entries(personnes).forEach(([id, personne]) => {
     const row = [
       personne.nom,
       personne.prenom,
       personne.coordonnees,
-      personne.binome,
-      personne.coordonnees_binome,
-      ...(assignments[id] || [])
+      personne.binome == 1 ? "Oui" : "Non",
+      personne.binome == 1 ? personne.coordonnees_binome : "",
+      ...(assignmentsPremium[id] || [])
+        .concat(Array(NBR_SORTIES).fill(""))
+        .slice(0, NBR_SORTIES),
+      ...(assignmentsAutres[id] || [])
         .concat(Array(NBR_SORTIES).fill(""))
         .slice(0, NBR_SORTIES),
     ];
-    csvRows.push(row);
+    data.push(row);
   });
 
-  // Conversion en format CSV
-  const csvContent = csvRows.map((row) => row.join(",")).join("\n");
+  generateExcelFile(data, "fichier5.xlsx", "downloadLink");
 
-  // Ajout du lien de téléchargement
-  const downloadLink = document.getElementById("downloadLink");
-  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-  const url = URL.createObjectURL(blob);
-  downloadLink.setAttribute("href", url);
-  downloadLink.setAttribute("download", "personnes_sorties.csv");
-  downloadLink.style.display = "block";
+  generateRecapInscriptionExcel(
+    { ...participantsByEventPremium, ...participantsByEventAutres },
+    "recap_inscriptions.xlsx",
+    "downloadLinkEventRecap"
+  );
+
+  generateRecapInscriptionExcel(
+    { ...waitingListPremium, ...waitingListAutres },
+    "liste_attente.xlsx",
+    "downloadLinkWaitingList"
+  );
+
+  function generateRecapInscriptionExcel(list, fileName, id) {
+    const data = []; // Initialisation des données pour le fichier Excel
+
+    // Ajouter les titres des événements sur la première ligne
+    const headerRow = Object.keys(list);
+    data.push(headerRow);
+
+    // Préparer les informations des participants
+    const maxParticipantsPerEvent = Math.max(
+      ...Object.values(list).map((participants) => participants.length)
+    );
+
+    // Pour chaque ligne, ajouter les participants sous chaque événement
+    for (let i = 0; i < maxParticipantsPerEvent; i++) {
+      const row = headerRow.map((eventName) => {
+        const participants = list[eventName] || [];
+        if (participants[i]) {
+          const person = personnes[participants[i]];
+          return formatParticipantInfo(person);
+        }
+        return ""; // Cellule vide si aucun participant
+      });
+      data.push(row);
+    }
+
+    // Générer le fichier Excel
+    generateExcelFile(data, fileName, id);
+  }
+
+  // Fonction utilitaire pour formater les informations des participants
+  function formatParticipantInfo(person) {
+    const lineBreak = `
+`;
+    const binomeInfo =
+      person.binome == 1
+        ? ` \nBinôme : ${person.binome} \nCoordonnées Binôme : ${person.coordonnees_binome}`
+        : "";
+    return `${person.nom} ${person.prenom} \nCoordonnées : ${person.coordonnees}${binomeInfo}`;
+  }
+}
+
+function generateExcelFile(data, fileName, downloadLinkId) {
+  const ExcelJS = window.ExcelJS; // Charger ExcelJS
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("Feuille 1");
+
+  // Ajouter des données et des styles
+  data.forEach((row, rowIndex) => {
+    const rowObj = worksheet.addRow(row);
+    rowObj.eachCell((cell) => {
+      if (
+        cell.value &&
+        typeof cell.value === "string" &&
+        cell.value.includes("\n")
+      ) {
+        // Remplacer \n par sa version compatible Excel
+        cell.value = cell.value.replace(/\n/g, "\n"); // Garder tel quel, ExcelJS gère cela
+        cell.alignment = { wrapText: true }; // Activer le wrapText pour afficher correctement
+      }
+    });
+
+    if (rowIndex === 0) {
+      // Style pour la première ligne (en-têtes)
+      rowObj.eachCell((cell) => {
+        cell.font = { bold: true, color: { argb: "FFFFFF" } };
+        cell.fill = {
+          type: "pattern",
+          pattern: "solid",
+          fgColor: { argb: "4CAF50" },
+        };
+        cell.alignment = {
+          vertical: "middle",
+          horizontal: "center",
+          wrapText: true,
+        };
+      });
+    }
+  });
+
+  // Ajuster la largeur des colonnes automatiquement
+  worksheet.columns.forEach((column) => {
+    let maxLength = 0;
+    column.eachCell({ includeEmpty: true }, (cell) => {
+      const cellValue = cell.value ? cell.value.toString() : "";
+      if (cellValue.includes("\n")) {
+        // Diviser en lignes et prendre la longueur maximale
+        const lines = cellValue.split("\n");
+        const longestLine = Math.max(...lines.map((line) => line.length));
+        maxLength = Math.max(maxLength, longestLine);
+      } else {
+        maxLength = Math.max(maxLength, cellValue.length);
+      }
+    });
+    column.width = maxLength + 2; // +2 pour une marge visuelle
+  });
+
+  // Figer les en-têtes
+  worksheet.views = [{ state: "frozen", ySplit: 1 }];
+
+  // Télécharger le fichier Excel
+  workbook.xlsx.writeBuffer().then((buffer) => {
+    const blob = new Blob([buffer], { type: "application/octet-stream" });
+    const downloadLink = document.getElementById(downloadLinkId);
+    downloadLink.setAttribute("href", URL.createObjectURL(blob));
+    downloadLink.setAttribute("download", fileName);
+    downloadLink.style.display = "block";
+  });
 }
